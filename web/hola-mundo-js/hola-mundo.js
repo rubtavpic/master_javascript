c91f3bc7f5892de9db87feda18b9bb77335f4f65
rubtavpic <rubentavero@outlook.com>
2019-04-28 01:05:08 +0200

    Clase 18 terminada

--- web/hola-mundo-js/hola-mundo.js
+++
@@ -1,4 +0,0 @@
-alert("Hola Mundo con JS");
-alert("Bienvenido al curso");
-document.write("Hola mundo desde fichero externo");
-console.log("Muestra esto en la consola");
---
+++ web/hola-mundo-js/js/01-hola-mundo.js
@@ -0,0 +1,5 @@
+alert("Hola Mundo con JS");
+alert("Bienvenido al curso");
+
+console.log("Muestra esto en la consola");
+console.log(88+4);
