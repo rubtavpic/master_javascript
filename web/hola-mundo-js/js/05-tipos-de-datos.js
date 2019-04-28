b016106ed6323ac9c385f3629935c4ae61e040b0
rubtavpic <rubentavero@outlook.com>
2019-04-28 13:11:36 +0200

    Clase 22 terminada

---
+++ web/hola-mundo-js/js/05-tipos-de-datos.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// Operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+
+alert('El resultado de la operación es: ' + operacion);
+
+// Tipos de datos
+var numero_entero = 44;
+var cadena_texto = 'Hola "que" tal';
+var verdadero_o_falso = false;
+
+var numero_falso = "33.4";
+
+console.log(String(numero_entero)+4);
