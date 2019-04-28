8814651190ae6db2d88c6f6e601d9481b7fed1da
rubtavpic <rubentavero@outlook.com>
2019-04-28 02:01:47 +0200

    Clase 21 terminada

---
+++ web/hola-mundo-js/js/04-constantes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// CONSTANTES
+// Como una variable pero su valor no puede cambiar
+
+var web = "htts://victorroblesweb.es";
+const ip = "192.88.0.12";
+
+web = "htts://victorroblescursos.es";
+// ip = "22232"; -> No dejaría hacerlo, una constante no puede cambiar
+
+console.log(web, ip);
